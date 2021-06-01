828b7b14c7e0333f2978179e6e842812458e4e5e
Jace <bellcastle88@gmail.com>
2021-06-01 20:46:53 +0900

    Commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,25 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-    return (
-        // 스타일 지정 
-        <button style={{ backgroundColor: color }} 
-        className='btn'>
-            {text}
-        </button>
-    )
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-    color: 'steelblue',
+  color: 'steelblue',
 }
 
 Button.propTypes = {
-    text: PropTypes.string,
-    color: PropTypes.string,
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,36 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({title}) => {
-    return ( 
-        <header className='header'>
-            <h1>{title}</h1>
-            {/* Button 재사용가능 */} 
-            <Button color='green' text='click'/>
-        </header>
-    )
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
 
-// defaultProps설정
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: 'Task Tracker',
 }
 
-// propTypes 지정
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
 
 export default Header
